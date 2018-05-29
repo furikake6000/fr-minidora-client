b193f1797733aed4ac01dece11f0a7fae179482d
furikake6000 <ewave63@keio.jp>
2018-05-29 17:19:23 +0900

    移動と修正

--- modules/actions/action.py
+++ modules/actions/action.py
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
-from abc import ABCMeta, abstractmethod
-
 class Action(object):
     def __init__(self):
-        pass
+        self.active = False
 
-    @abstractmethod
     def activate(self):
         # Called when action activated
-        pass
+        self.active = True
     
-    @abstractmethod
     def update(self):
         # Called every frame while action is activated
         pass
 
-    @abstractmethod
     def deactivate(self):
         # Called when action deactivated
-        pass
+        self.active = False
